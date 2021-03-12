aa7eb46d81186295f63ad7e705be0c1c2dc062f6
pete3249 <katelynrpeterson16@gmail.com>
2021-03-12 13:38:55 -0500

    Add components and containers

---
+++ src/containers/EventIndexContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class EventIndexContainer extends React.Component {
+    render() {
+        return <div>Inside Event Index Container</div>
+    }
+}
+
+export default EventIndexContainer;
---
+++ src/containers/RestaurantIndexContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class RestaurantIndexContainer extends React.Component {
+    render() {
+        return <div>Inside Restaurant Index Container</div>
+    }
+}
+
+export default RestaurantIndexContainer;
