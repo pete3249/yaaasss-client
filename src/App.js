b2671f65733d1b5023dee5654268e99597fa9773
pete3249 <katelynrpeterson16@gmail.com>
2021-03-12 14:10:15 -0500

    Add navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,43 @@ import {
   BrowserRouter as Router, 
   Switch,
   Route,
-  Link
+  NavLink
 } from 'react-router-dom';
+import RestaurantIndexContainer from './containers/RestaurantIndexContainer';
+import EventIndexContainer from './containers/EventIndexContainer';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div>
+      <Router>
+        <nav className="text-center bg-blue-900 text-yellow-100 p-4">
+          <NavLink 
+            className="inline-block px-4 p-2" 
+            activeClassName="text-yellow-300"
+            exact
+            to="/"
+            >
+              Home
+          </NavLink>
+          <NavLink 
+            className="inline-block px-4 p-2" 
+            activeClassName="text-yellow-300"
+            to="/restaurants"
+            >
+              Search
+          </NavLink>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <EventIndexContainer />
+          </Route>
+          <Route path="/restaurants">
+            <RestaurantIndexContainer />
+          </Route>
+          <Route path="/events/new">Create New Event</Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
