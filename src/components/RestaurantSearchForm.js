e6e74ac94790e88a18b939f0d2b87c2964c45f29
pete3249 <katelynrpeterson16@gmail.com>
2021-03-12 20:26:20 -0500

    Add fetch request for form submission

--- src/components/RestaurantSearchForm.js
+++ src/components/RestaurantSearchForm.js
@@ -1,12 +1,53 @@
 import React from 'react';
 
 class RestaurantSearchForm extends React.Component {
+    state = {
+        categories: '',
+        location: ''
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    handleOnSubmit = e => {
+        e.preventDefault();
+        fetch(`http://localhost:3001/restaurant_search/?categories=${this.state.categories}&location=${this.state.location}`, {
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            }
+        })
+    }
+    
     render() {
         return (
-            <div> Search For Restaurants!
-                <form>
-                    <input type="text" />
-                    <input type="submit" />
+            <div>
+                <form onSubmit={this.handleOnSubmit}>
+                <h1 className="text-center text-3xl font-semibold mb-2">Find a Local Restaurant</h1>
+                    <fieldset>
+                        <input
+                            type="text"
+                            name="categories"
+                            onChange={this.handleChange}
+                            value={this.state.category}
+                            placeholder="Choose a category"
+                            className="w-full border p-4 my-4"
+                        />
+                    </fieldset>
+                    <fieldset>
+                        <input
+                            type="text"
+                            name="location"
+                            onChange={this.handleChange}
+                            value={this.state.location}
+                            placeholder="Enter a zip code"
+                            className="w-full border p-4 my-4"
+                        />
+                    </fieldset>
+                    <button className="w-full p-4 bg-blue-300 mt-4 hover:bg-blue-400 transition-all duration-200" type="submit">Search</button>
                 </form>
             </div>
         )
