b97330b11ebbfdeb93ff98f6a6d80c42b6b3f8e8
pete3249 <katelynrpeterson16@gmail.com>
2021-03-15 11:41:42 -0400

    Add button to new event page from restaurant

--- src/components/RestaurantListItem.js
+++ src/components/RestaurantListItem.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
-const RestaurantListItem = ({restaurant}) => {
+const RestaurantListItem = ({ restaurant }) => {
     return (
         <div className="lg:m-4 shadow-md hover:shadow-lg hover:bg-yellow-200 rounded-lg bg-white my-12 mx-8 max-w-2xl mx-auto mt-10">
             <img src={restaurant.image_url} alt={restaurant.alias} className="object-scale-down h-64 w-full overflow-hidden pt-2"></img>
             <div className="p-4">
                 <h3 className="font-medium text-gray-600 text-lg my-2 uppercase">{restaurant.name}</h3>
                 <ul className="text-justify">
-                    {restaurant.categories.map(category => <li className="list-disc list-inside">{category.title}</li>)}
+                    {restaurant.categories.map(category => <li key={category.alias} className="list-disc list-inside">{category.title}</li>)}
                     <li>Rating: {restaurant.rating}</li>
                     <li>Price: {restaurant.price}</li>
                     <li>Address: {restaurant.location.address1}</li>
                 </ul>
-                <div className="mt-5">
-                    <a href={restaurant.url} className="hover:bg-gray-700 rounded-full py-2 px-3 font-semibold hover:text-white bg-gray-400 text-gray-100">Learn More</a>
+                <div className="flex justify-evenly mt-5">
+                    <a href={restaurant.url} className="hover:bg-gray-700 rounded-full py-2 px-3 font-semibold hover:text-white bg-gray-400 text-gray-100">Learn more</a>
+                    <a href="/events/new" className="hover:bg-gray-700 rounded-full py-2 px-3 font-semibold hover:text-white bg-gray-400 text-gray-100"> Create event</a>
                 </div>
             </div>
         </div>
