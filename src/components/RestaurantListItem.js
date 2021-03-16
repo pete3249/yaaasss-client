14c348c5e39e3eff62b797b80a9900f1943309c8
pete3249 <katelynrpeterson16@gmail.com>
2021-03-16 11:44:42 -0400

    Add state and styling

--- src/components/RestaurantListItem.js
+++ src/components/RestaurantListItem.js
@@ -1,24 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const RestaurantListItem = ({ restaurant }) => {
-    return (
-        <div className="lg:m-4 shadow-md hover:shadow-lg hover:bg-yellow-200 rounded-lg bg-white my-12 mx-8 max-w-2xl mx-auto mt-10">
-            <img src={restaurant.image_url} alt={restaurant.alias} className="object-scale-down h-64 w-full overflow-hidden pt-2"></img>
-            <div className="p-4">
-                <h3 className="font-medium text-gray-600 text-lg my-2 uppercase">{restaurant.name}</h3>
-                <ul className="text-justify">
-                    {restaurant.categories.map(category => <li key={category.alias} className="list-disc list-inside">{category.title}</li>)}
-                    <li>Rating: {restaurant.rating}</li>
-                    <li>Price: {restaurant.price}</li>
-                    <li>Address: {restaurant.location.address1}</li>
-                </ul>
-                <div className="flex justify-evenly mt-5">
-                    <a href={restaurant.url} className="hover:bg-gray-700 rounded-full py-2 px-3 font-semibold hover:text-white bg-gray-400 text-gray-100">Learn more</a>
-                    <a href="/events/new" className="hover:bg-gray-700 rounded-full py-2 px-3 font-semibold hover:text-white bg-gray-400 text-gray-100"> Create event</a>
+class RestaurantListItem extends React.Component {
+    state = {
+        restaurant: this.props.restaurant
+    }
+    
+    render() {
+        return (
+            <div className="lg:m-4 shadow-md hover:shadow-lg hover:bg-gray-200 rounded-lg bg-white my-12 mx-8 max-w-2xl mx-auto mt-10">
+                <img src={this.state.restaurant.image_url} alt={this.state.restaurant.alias} className="object-scale-down h-64 w-full overflow-hidden"></img>
+                <div className="p-2">
+                    <h3 className="text-center font-medium text-gray-600 text-lg my-2 uppercase underline">{this.state.restaurant.name}</h3>
+                    <ul className="text-justify px-4">
+                        {this.state.restaurant.categories.map(category => <li key={category.alias} className="list-disc list-inside">{category.title}</li>)}
+                        <li>Rating: {this.state.restaurant.rating}</li>
+                        <li>Price: {this.state.restaurant.price}</li>
+                        <li>Address: {this.state.restaurant.location.address1}</li>
+                    </ul>
+                    <div className="flex justify-evenly mt-5">
+                        <a href={this.state.restaurant.url} className="bg-gray-900 text-white px-3 py-3 rounded rounded-md text-sm font-large hover:bg-purple-500 border my-3">Learn more</a>
+                        <Link to={{
+                            pathname:"/events/new",
+                            state: {restaurant: this.state.restaurant}
+                        }}
+                        className="btn btn-primary bg-gray-900 text-white px-3 py-3 rounded rounded-md text-sm font-large hover:bg-purple-500 border my-3"
+                        >
+                            Create event
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default RestaurantListItem;
