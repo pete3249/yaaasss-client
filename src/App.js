05a61017d629f1f47b49b0392bdfe50cbbd134c1
pete3249 <katelynrpeterson16@gmail.com>
2021-03-16 10:52:13 -0400

    Change navbar styling

--- src/App.js
+++ src/App.js
@@ -7,36 +7,43 @@ import {
 } from 'react-router-dom';
 import RestaurantIndexContainer from './containers/RestaurantIndexContainer';
 import EventIndexContainer from './containers/EventIndexContainer';
+import NewEventContainer from './containers/NewEventContainer';
 
 function App() {
   return (
     <div>
       <Router>
-        <nav className="text-center bg-blue-900 text-yellow-100 p-4">
-          <NavLink 
-            className="inline-block px-4 p-2" 
-            activeClassName="text-yellow-300"
-            exact
-            to="/"
-            >
-              Home
-          </NavLink>
-          <NavLink 
-            className="inline-block px-4 p-2" 
-            activeClassName="text-yellow-300"
-            to="/restaurants"
-            >
-              Search
-          </NavLink>
+        <nav className="bg-gray-800 text-center text-white p-4">
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start"></div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <h1 className="text-purple-400 px-3 py-2 rounded-md text-2xl font-medium"> yaaasss </h1>
+                <NavLink 
+                  className="bg-gray-900 text-white px-4 py-3 rounded-md text-sm font-large" 
+                  activeClassName="text-purple-200"
+                  exact
+                  to="/"
+                  >
+                    Home
+                </NavLink>
+                <NavLink 
+                  className="bg-gray-900 text-white px-4 py-3 rounded-md text-sm font-xl" 
+                  activeClassName="text-purple-200"
+                  to="/restaurants"
+                  >
+                   Search
+                </NavLink>
+            </div>
+          </div>
         </nav>
         <Switch>
-          <Route exact path="/">
-            <EventIndexContainer />
+          <Route exact path="/" >
+            <EventIndexContainer/>
           </Route>
           <Route path="/restaurants">
-            <RestaurantIndexContainer />
+            <RestaurantIndexContainer/>
           </Route>
-          <Route path="/events/new">Create New Event</Route>
+          <Route path="/events/new" render={(routerProps => <NewEventContainer {...routerProps}/>)}/>
         </Switch>
       </Router>
     </div>
