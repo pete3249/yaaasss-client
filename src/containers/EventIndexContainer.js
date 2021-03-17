03ee76da3c79f136ebb3f5fb46ba46fea016082e
pete3249 <katelynrpeterson16@gmail.com>
2021-03-17 08:46:08 -0400

    Add conditional logic and lifecycle method

--- src/containers/EventIndexContainer.js
+++ src/containers/EventIndexContainer.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 class EventIndexContainer extends React.Component {
+    state = {
+        events: [],
+        loading: true
+    }
+
+    componentDidMount() {
+
+    }
+
     render() {
+        if(this.state.loading) {
+            return <div>Loading spinner</div>
+        }
         return <div>Inside Event Index Container</div>
     }
 }
