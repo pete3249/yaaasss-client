d8f5bbe21c3247067a5dd191b8cc37f2be1f8454
pete3249 <katelynrpeterson16@gmail.com>
2021-03-17 08:44:31 -0400

    Add createStore to seperate file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import store from './store';
 import { Provider } from 'react-redux';
-import restaurantReducer from './reducers/restaurant_reducer';
 import App from './App';
 
-const store = createStore(restaurantReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
 ReactDOM.render(
-  <Provider store={store}>
-     <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
