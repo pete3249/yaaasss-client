cdbfe9fb82a525a80169e952726cf5c876a48aff
pete3249 <katelynrpeterson16@gmail.com>
2021-03-18 12:47:32 -0400

    Add usersReducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import restaurantReducer from './restaurant_reducer';
+import userReducer from './user_reducer';
 
 export default combineReducers({
-    restaurants: restaurantReducer
+    restaurants: restaurantReducer,
+    users: userReducer
 });
