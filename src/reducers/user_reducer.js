617f95bfe825faee9bc6232249327ccbf543ce0f
pete3249 <katelynrpeterson16@gmail.com>
2021-03-18 12:47:15 -0400

    Add usersReducer

---
+++ src/reducers/user_reducer.js
@@ -0,0 +1,24 @@
+import { 
+    START_LOADING_USERS, 
+    ADD_USERS, 
+    SUCCESSFULLY_LOADED_USERS
+} from '../actions/index'
+
+const initialState = {
+    loadingState: 'notStarted',
+    list: []
+}
+
+export default function userReducer(state = initialState, action) {
+    switch (action.type) {
+        case START_LOADING_USERS:
+            return {...state, loadingState: 'inProgress'}
+        case ADD_USERS:
+            return {...state, list: action.payload}
+        case SUCCESSFULLY_LOADED_USERS:
+            return {...state, loadingState: 'successful'}
+        default:
+             return state   
+    }
+}
+
