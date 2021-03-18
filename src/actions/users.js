5215196c1b436e239bbe40f531041ca3a2fc2f53
pete3249 <katelynrpeterson16@gmail.com>
2021-03-18 12:48:11 -0400

    Add fetchUsers to use for newEventForm

---
+++ src/actions/users.js
@@ -0,0 +1,27 @@
+import { 
+    START_LOADING_USERS,
+    ADD_USERS,
+    SUCCESSFULLY_LOADED_USERS
+} from '.'
+
+export const fetchUsers = () => {
+    return (dispatch) => {
+        dispatch({type: START_LOADING_USERS})
+        fetch('http://localhost:3001/users', {
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(usersJSON => {
+            dispatch({
+                type: ADD_USERS,
+                payload: usersJSON
+            })
+        })
+        .then(() => {
+            dispatch({type: SUCCESSFULLY_LOADED_USERS})
+        })
+    }
+}
