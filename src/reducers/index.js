471f3442962368e03ceff5dc4194dd2c71f56b9a
pete3249 <katelynrpeterson16@gmail.com>
2021-03-21 00:02:37 -0400

    Remove unneeded reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import restaurantReducer from './restaurant_reducer';
 import userReducer from './user_reducer';
-import eventReducer from './event_reducer';
 
 export default combineReducers({
     restaurants: restaurantReducer,
-    users: userReducer,
-    events: eventReducer
+    users: userReducer
 });
