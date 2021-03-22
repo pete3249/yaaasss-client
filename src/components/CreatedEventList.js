4ed0ee2eef82b9d32c8a16ce10dc56f65afaed86
pete3249 <katelynrpeterson16@gmail.com>
2021-03-22 11:37:01 -0400

    Format created event list

--- src/components/CreatedEventList.js
+++ src/components/CreatedEventList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import EventListItem from './EventListItem';
 
-const CreatedEventList = ({events}) => {
+const CreatedEventList = ({events, handleDeleteEvent}) => {
     return (
         <div>
-            <h1 className="text-gray-900 text-center text-3xl font-semibold pt-4">My Events</h1>
-          {events.map(event => <EventListItem key={event.id} event={event} />)}
+            <h1 className="text-gray-900 text-center text-3xl font-semibold pt-4 pb-4">My Events</h1>
+            <div className="grid grid-cols-2 gap-4">
+                {events.map(event => <EventListItem key={event.id} event={event} handleDeleteEvent={handleDeleteEvent} />)}
+            </div>
         </div>
     )
 }
