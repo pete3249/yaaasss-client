1bcdfbfe51a4991a15d4e9deac9be165273592d7
pete3249 <katelynrpeterson16@gmail.com>
2021-03-23 01:05:01 -0400

    Make grid 3 columns

--- src/components/CreatedEventList.js
+++ src/components/CreatedEventList.js
@@ -5,7 +5,7 @@ const CreatedEventList = ({events, handleDeleteEvent}) => {
     return (
         <div>
             <h1 className="text-gray-900 text-center text-3xl font-semibold pt-6 pb-6">My Events</h1>
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-3 gap-4">
                 {events.map(event => <EventListItem key={event.id} event={event} handleDeleteEvent={handleDeleteEvent} />)}
             </div>
         </div>
