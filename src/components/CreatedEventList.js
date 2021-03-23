a54d14d4abcfe303770c430749fdaa2b504fe277
pete3249 <katelynrpeterson16@gmail.com>
2021-03-22 21:32:57 -0400

    Add styling to ternary expression

--- src/components/CreatedEventList.js
+++ src/components/CreatedEventList.js
@@ -4,7 +4,7 @@ import EventListItem from './EventListItem';
 const CreatedEventList = ({events, handleDeleteEvent}) => {
     return (
         <div>
-            <h1 className="text-gray-900 text-center text-3xl font-semibold pt-4 pb-4">My Events</h1>
+            <h1 className="text-gray-900 text-center text-3xl font-semibold pt-6 pb-6">My Events</h1>
             <div className="grid grid-cols-2 gap-4">
                 {events.map(event => <EventListItem key={event.id} event={event} handleDeleteEvent={handleDeleteEvent} />)}
             </div>
