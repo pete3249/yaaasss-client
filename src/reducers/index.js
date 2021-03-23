0b6a59549b36b237b0cfbf9c59ef480b52476382
pete3249 <katelynrpeterson16@gmail.com>
2021-03-23 01:00:36 -0400

    Add userEvent Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import restaurantReducer from './restaurant_reducer';
 import userReducer from './user_reducer';
 import eventReducer from './event_reducer';
+import userEventReducer from './user_event_reducer';
 
 export default combineReducers({
     restaurants: restaurantReducer,
     users: userReducer,
-    events: eventReducer
+    events: eventReducer,
+    userEvents: userEventReducer
 });
