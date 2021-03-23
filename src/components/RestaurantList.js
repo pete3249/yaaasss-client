e31a6d6e0c7582df77dbcc8ceb4e420f43609497
pete3249 <katelynrpeterson16@gmail.com>
2021-03-23 01:14:47 -0400

    Add conditional logic

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -4,7 +4,7 @@ import RestaurantListItem from './RestaurantListItem';
 const RestaurantList = ({restaurants}) => {
     return (
       <div className="grid grid-cols-3 gap-4">
-          {restaurants.map(restaurant => <RestaurantListItem key={restaurant.id} restaurant={restaurant}/>)}
+          {restaurants? restaurants.map(restaurant => <RestaurantListItem key={restaurant.id} restaurant={restaurant}/>): ''}
       </div>
     );
 };
